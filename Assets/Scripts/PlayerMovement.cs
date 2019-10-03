0fd4e55f482c61830ce8f25f91c4b3a2bb48265a
Matrigs <mtrigsauer@gmail.com>
2019-10-02 21:31:05 -0300

    Initial commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+	public float moveSpeed = 5f;
+
+	public Rigidbody2D playerRB;
+	public Animator playerAnim;
+
+	Vector2 movement;
+
+    // Update is called once per frame
+    void Update() {
+		movement.x = Input.GetAxisRaw("Horizontal");
+		movement.y = Input.GetAxisRaw("Vertical");
+
+		playerAnim.SetFloat("Horizontal", movement.x);
+		playerAnim.SetFloat("Vertical", movement.y);
+		playerAnim.SetFloat("Speed", movement.sqrMagnitude);
+	}
+
+	void FixedUpdate() {
+		playerRB.MovePosition(playerRB.position + movement * moveSpeed * Time.fixedDeltaTime);
+	}
+}
