209cf1c63d4c713aa6091edd536ba6040520aec2
Matrigs <mtrigsauer@gmail.com>
2019-10-07 18:14:17 -0300

    Cut one head, two grow back
    Enemy respawn working

---
+++ Assets/Scripts/HydraBehavior.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HydraBehavior : MonoBehaviour
+{
+	public List<SpawnPoint> spawnHydras;
+
+	void OnCollisionEnter2D(Collision2D collision) {
+		if (collision.gameObject.tag == "Bullet") {
+
+			for (int i = 0; i < spawnHydras.Count; i++) {
+				spawnHydras[i].Respawn();
+			}
+			Destroy(gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/SpawnPoint.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPoint : MonoBehaviour
+{
+	public Transform spawnPoint;
+	public GameObject enemyPrefab;
+	IEnumerator coroutine;
+
+	public void Respawn () {
+		GameObject newEnemy = Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+		coroutine = WaitTime();
+		StartCoroutine(coroutine);
+	}
+
+	IEnumerator WaitTime () {
+		yield return new WaitForSeconds(1);
+	}
+}
