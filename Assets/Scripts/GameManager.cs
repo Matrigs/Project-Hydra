4900b234c2bed435a4be09364882c0a91bfde324
Matrigs <mtrigsauer@gmail.com>
2019-10-10 16:02:58 -0300

    Death and game over UI placeholders

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+	public GameObject gameOverMenu;
+
+	void Start() {
+		gameOverMenu.SetActive(false);
+	}
+
+	public void EndGame() {
+		Debug.Log("GAME OVER");
+		Restart();
+	}
+
+	void Restart() {
+		gameOverMenu.SetActive(true);
+	}
+}
---
+++ Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+	public PlayerMovement movementScript;
+
+	void OnCollisionEnter2D(Collision2D collisionInfo) {
+		if (collisionInfo.collider.tag == "Enemy") {
+			movementScript.enabled = false;
+			FindObjectOfType<GameManager>().EndGame();
+		}
+	}
+}
