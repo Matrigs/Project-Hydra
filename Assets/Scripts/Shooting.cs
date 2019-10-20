5d94059a0d28b5cd2afab617c01766a0e74d1a10
Matrigs <mtrigsauer@gmail.com>
2019-10-20 16:20:25 -0300

    Main Menu, Shooting works as an orb
    Implemented Main Menu, cooldowns + Orb-like shooting (including trail) and Pause Menu's panel

--- Assets/Scripts/Shooting.cs
+++ Assets/Scripts/Shooting.cs
@@ -5,20 +5,39 @@ using UnityEngine;
 public class Shooting : MonoBehaviour
 {
 	public Transform firePoint;
+	public GameObject wand;
 	public GameObject bulletPrefab;
+
+	IEnumerator coroutine;
+	public bool shotFired = false;
 
 	public float bulletForce = 20f;
 
     // Update is called once per frame
     void Update() {
-        if(Input.GetButtonDown("Fire1")) {
-			Shoot();
+		if (shotFired == false) {
+			if (Input.GetButtonDown("Fire1")) {
+				Shoot();
+			}
 		}
     }
 
 	void Shoot () {
+		wand.SetActive(false);
 		GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
 		Rigidbody2D bulletRB = bullet.GetComponent<Rigidbody2D>();
 		bulletRB.AddForce(firePoint.up * bulletForce, ForceMode2D.Impulse);
+
+		//Iniciar cooldown do ataque
+		shotFired = true;
+		coroutine = Cooldown();
+		StartCoroutine (coroutine);
+	}
+
+	IEnumerator Cooldown () {
+		//Depois do cooldown, respawna a orbe
+		yield return new WaitForSeconds(3);
+		wand.SetActive(true);
+		shotFired = false;
 	}
 }
