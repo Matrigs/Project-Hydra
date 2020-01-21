e369ca49636ac2e16336457e92b76429e0604ad7
Matrigs <mtrigsauer@gmail.com>
2020-01-21 14:58:26 -0300

    Hydra animator controller now set to pathfinder
    + Level Design sketches and tileset updates

--- Assets/Scripts/HydraBehavior.cs
+++ Assets/Scripts/HydraBehavior.cs
@@ -1,10 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Pathfinding;
 
 public class HydraBehavior : MonoBehaviour
 {
+	public AIPath aiPath;
+	public Animator hydraAnim;
 	public List<SpawnPoint> spawnHydras;
+
+	void Start() {
+		hydraAnim.SetFloat("Speed", 1);
+	}
+
+	void Update() {
+		if (aiPath.desiredVelocity.x != 0f) {
+			hydraAnim.SetFloat("Horizontal", aiPath.velocity.x);
+		}
+
+		if (aiPath.desiredVelocity.y != 0f) {
+			hydraAnim.SetFloat("Vertical", aiPath.velocity.y);
+		}
+	}
 
 	void OnCollisionEnter2D(Collision2D collision) {
 		if (collision.gameObject.tag == "Bullet") {
