d80253d787bb6a36012d61d5aa45f97a03e7fcfa
Matrigs <mtrigsauer@gmail.com>
2020-01-28 17:36:56 -0300

    Aggro finalized... for now.

---
+++ Assets/Scripts/AggroZone.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class AggroZone : MonoBehaviour {
+
+	public GameObject hydraPrefab;
+	public Transform playerPrefab;
+	public AIDestinationSetter aIDestination;
+
+	void Start() {
+		aIDestination = hydraPrefab.GetComponent<AIDestinationSetter>();
+		aIDestination.target = null;
+	}
+
+	void OnTriggerEnter2D(Collider2D collision) {
+		if (collision.CompareTag("Player")) {
+			Debug.Log("Oh? You're approaching me?");
+			aIDestination.target = playerPrefab;
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D collision) {
+		if (collision.CompareTag("Player")) {
+			Debug.Log("Oh? Running away?");
+			StartCoroutine("AggroCountdown");
+		}
+	}
+
+	IEnumerator AggroCountdown () {
+		yield return new WaitForSeconds(1);
+		Debug.Log("Waiting...");
+		StartCoroutine("EndAggro");
+	}
+
+	IEnumerator EndAggro () {
+		aIDestination.target = null;
+		Debug.Log("Not interested anymore.");
+		yield return null;
+	}
+}
